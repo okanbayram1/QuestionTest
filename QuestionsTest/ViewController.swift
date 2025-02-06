51c5816dc6b1dc053ec600f5aecd5f76bd53107b
Okan Bayram <okanbayram85@icloud.com>
2025-02-06 17:05:16 +0300

    Initial Commit

---
+++ QuestionsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionsTest
+//
+//  Created by okan bayram on 6.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
